9f54ed78d41158a53f4963a17f8ce1f2ecbba1ac
maaatsu <bigbear.honey.87@gmail.com>
2023-08-28 16:39:20 +0900

    messageテストコード実装

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージが投稿できる場合' do
+      it 'contentとimageが存在していれば保存できる' do
+        expect(@message).to be_valid
+      end
+      it 'contentが空でも保存できる' do
+        @message.content = ''
+        expect(@message).to be_valid
+      end
+      it 'imageが空でも保存できる' do
+        @message.image = nil
+        expect(@message).to be_valid
+      end
+    end
+    context 'メッセージが投稿できない場合' do
+      it 'contentとimageが空では保存できない' do
+        @message.content = ''
+        @message.image = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("メッセージ を入力してください")
+      end
+      it 'roomが紐付いていないと保存できない' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include('チャットルーム は存在しないチャットルームです')
+      end
+      it 'userが紐付いていないと保存できない' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include('ユーザー名 は存在しないユーザーです')
+      end
+    end
+  end
 end
